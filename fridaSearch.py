1e6b2299cb45f85230ff7eb22c05541bb580c4d0
phrantom <52974841+phrantom@users.noreply.github.com>
2024-01-15 11:44:45 -0300

    Update fridaSearch.py
    Mejora del codigo

--- fridaSearch.py
+++ fridaSearch.py
@@ -1,11 +1,14 @@
+
 import requests
 from bs4 import BeautifulSoup
 from colorama import Fore
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description='Frida CodeShare Search')
-parser.add_argument('-s', '--Search',type=str, help='Search word')
-parser.add_argument('-a','-all', help='All frida codes')
+parser.add_argument('-s', '--search', type=str, help='Search word')
+parser.add_argument('-a', '--all', action='store_true', help='All frida codes')
+parser.add_argument('-o', '--output', type=str, help='Output file in JSON format')
 args = parser.parse_args()
 
 
@@ -22,50 +25,63 @@ def logo():
                    Frida CodeShare Search                         """ + Fore.LIGHTBLACK_EX + """ 
                                                       by phr4nt0m """ + Fore.RESET )
 
-def all_articles():
-    article_dic = list()
-    for page in range(1,23): #23
-        url = "https://codeshare.frida.re/browse?page=" + str(page)
 
-        r = requests.get(url)   
-        html = BeautifulSoup(r.text,'html.parser')
+def all_articles():
+    article_list = []
+
+    for page in range(1, 23):  # 23
+        url = f"https://codeshare.frida.re/browse?page={page}"
+        r = requests.get(url)
+        html = BeautifulSoup(r.text, 'html.parser')
         articles_html = html.find_all('article')
 
         for article in articles_html:
-            article_list = list()
-            article_list.append(article.a.text.lower()) # Title
-            article_list.append(article.h3.text.replace(" ","").replace("\n","")) # likes
-            article_list.append(article.p.text.lower())   # Description
-            article_list.append(article.a['href']) # Link
-            article_dic.append(article_list) 
-    return article_dic
+            title = article.a.text.lower()
+            likes = article.h3.text.replace(" ", "").replace("\n", "")
+            description = article.p.text.lower()
+            link = article.a['href']
+            article_list.append({"Name": title, "Likes": likes, "Description": description, "Url": link})
+
+    return article_list
+
 
 def search_word(word_search):
-    article_dic = all_articles()
-    list_found= list()
-    
-    for item in article_dic:
-        if word_search in item[0] or word_search in item[2]:
-            list_found.append(item)
-
+    article_list = all_articles()
+    list_found = [item for item in article_list if word_search in item["Name"] or word_search in item["Description"]]
     return list_found
 
 
 def print_dict(list_found):
-    if (len(list_found)>0):
+    if list_found:
         for art in list_found:
-            print(Fore.CYAN + "Name:      " + Fore.GREEN + art[0]+ Fore.RESET)
-            print("Likes:       " + Fore.RED + art[1]+ Fore.RESET)
-            print("Description: " + Fore.LIGHTBLACK_EX + art[2] + Fore.RESET)
-            print("Url:         " + Fore.LIGHTBLUE_EX + art[3] + Fore.RESET )
-            url = art[3]
-            print("Code Run:    " + Fore.LIGHTMAGENTA_EX + "frida -U --codeshare " + url[url.rfind("@")+1:-1] +" -f YOUR_BINARY\n" + Fore.RESET)
+            print(Fore.CYAN + f"Name:      {Fore.GREEN}{art['Name']}{Fore.RESET}")
+            print(f"Likes:       {Fore.RED}{art['Likes']}{Fore.RESET}")
+            print(f"Description: {Fore.LIGHTBLACK_EX}{art['Description']}{Fore.RESET}")
+            print(f"Url:         {Fore.LIGHTBLUE_EX}{art['Url']}{Fore.RESET}")
+            url = art['Url']
+            print(f"Code Run:    {Fore.LIGHTMAGENTA_EX}frida -U --codeshare {url[url.rfind('@')+1:-1]} -f YOUR_BINARY{Fore.RESET}\n")
     else:
         print(Fore.RED + " No Match Found !! ")
 
 
-if (args.Search != "" ):
-    logo()
-    print_dict(search_word(args.Search))
+def save_to_json(data, output_file):
+    with open(output_file, 'w') as file:
+        json.dump(data, file, indent=2)
 
+
+if args.search:
+    logo()
+    result = search_word(args.search)
+    print_dict(result)
+    if args.output:
+        save_to_json(result, args.output)
+elif args.all:
+    logo()
+    result = all_articles()
+    print_dict(result)
+    if args.output:
+        save_to_json(result, args.output)
+else:
+    logo()
+    parser.print_help()
 
